32b474f7a42774da20294c969ce5a261d33a3426
Robin <Robin.Steltmann@googlemail.com>
2017-12-13 22:40:04 +0100

    commit lib

--- index.php
+++ index.php
@@ -2,78 +2,113 @@
 
 require 'vendor/autoload.php';
 
-$unrct = new general\unreachablecodetool();
-if($_GET['area'] == "file") {
-    $proj = $_GET['proj'];
-    if($proj == ""){
-        $proj = "classcalls";
+$file = $_GET['file'];
+$proj = $_GET['proj'];
+if($proj == ""){
+    $proj = "classcalls";
+}
+$area = in_array($_GET['area'],array("file","goto","createnew"))?$_GET['area']:"";
+$ajax = in_array($_GET['ajax'],array("code","problem","file","projects"))?$_GET['ajax']:"";
+
+if($ajax == 'code') {
+    echo file_get_contents($file);
+    exit;
+}
+if($ajax == 'projects') {
+    $handle = opendir("uploads/");
+    if($handle) {
+        $projectsarray = array();
+        while (false !== ($file = readdir($handle))) {
+            if($file != ".." && $file != "."){
+                $projectsarray[] = $file;
+            }
+            
+        }
     }
+    echo json_encode($projectsarray);
+    exit;
+}
+
+if($area == "goto") {
+    header("location:http://localhost/bachelorarbeit/index.php?area=file&proj=".$_POST['project']);
+    exit;
+}
+if($area == "createnew") {
+    $projectname = $_POST['projectname'];
+    mkdir("uploads/".$projectname);
+    $count = 0;
+    foreach ($_FILES['files']['name'] as $i => $name) {
+        if (strlen($_FILES['files']['name'][$i]) > 1) {
+            if (move_uploaded_file($_FILES['files']['tmp_name'][$i], 'uploads/'.$projectname."/".$name)) {
+                $count++;
+            }
+        }
+    }
+    header("location:http://localhost/bachelorarbeit/index.php?area=file&proj=".$projectname);
+    exit;
+}
+if($ajax == 'problem') {
+    $classparser = new code\codeparser(file_get_contents($file),dirname($file));
+    
+    $location = "uploads/".$proj;
+    
+    $unrct = new general\unreachablecodetool();
+    $files = $unrct->read_recursiv($location, true);
+
+    $classcalls = $classparser->getClassCalls();
+    $classdeclarations = $classparser->getClassDeclarations();
+    
+    $functioncalls = $classparser->getFunctionCalls();
+    $functiondeclarations = $classparser->getFunctionDeclarations();
+    $errors = $classparser->getError();
+    
+    foreach($files as $onefile) {
+        if($onefile != $file) {
+            $code = file_get_contents($onefile);
+            $fileparser = new code\projectparser($code,dirname($onefile));
+            $classcalls = $fileparser->getClassCalls();   
+            $functioncalls = $fileparser->getFunctionCalls();   
+        }
+    }
+    
+    foreach($functiondeclarations as $key=>$onedeclaration) {
+        $diff = array_diff_key($onedeclaration,$functioncalls[$key]);
+        if(!empty($diff)) {
+            foreach($diff as $onediff){
+                $errors["nofunccall"][] = $onediff;
+            }
+        }
+    }
+    $diff = array_diff_key($classdeclarations,$classcalls);
+    if(!empty($diff)) {
+        foreach($diff as $onediff){
+            $errors["noclasscall"][] = $onediff;
+        }
+    }
+    echo json_encode($errors);
+    exit;
+}
+
+$unrct = new general\unreachablecodetool();
+if($area == "file") {
     $location = "uploads/".$proj;
     $files = $unrct->read_recursiv($location, true);
 
     foreach($files as $file) {
         $code = file_get_contents($file);
-        switch($_GET['area']) {
-            case "file":
-                $fileparser = new file\fileparser($code,dirname($file));
-                $filecalls[$file] = $fileparser->getFileCalls();
-                break;
-            case "class":
-                $classparser = new classparser($code,dirname($file));
-                $classcalls[substr(basename($file),0,-4)] = $classparser->getClassCalls();
-    //            $classcalls = createtree($classcalls,$classparser->getClassCalls(),substr(basename($file),0,-4));
-                break;
-            default:
-                break;
-        }
+        $fileparser = new file\fileparser($code,dirname($file));
+        $filecalls[$file] = $fileparser->getFileCalls();
     }
-    switch($_GET['area']) {
-        case "file":
-            $roots = $unrct->getroots($filecalls);
-            foreach(array_diff_key($filecalls,$roots) as $key=>$onediff){
-                $filecalls = $unrct->createtree($filecalls,$key);
-            }
-            $filevisualizer = new file\filevisualizer($filecalls);
-            break;
-        case "class":
-            //setup root
-            /*
-            $finalclasscalls[0] = array();
-            foreach($classcalls as $subclasscalls){
-                foreach($subclasscalls as $key=>$value){
-                    $finalclasscalls[$key] = $value;
-                }
-            }
-            foreach(array_diff_key($finalclasscalls,array(0)) as $key=>$onediff){
-                $finalclasscalls = createtree($finalclasscalls,$key);
-            }
-            print_r($finalclasscalls);
-            $classvisualizer = new classvisualizer($finalclasscalls);
-            break;
-             * */
-
-        default:
-            break;
+    $roots = $unrct->getroots($filecalls);
+    foreach(array_diff_key($filecalls,$roots) as $key=>$onediff){
+        $filecalls = $unrct->createtree($filecalls,$key);
     }
+    $filevisualizer = new file\filevisualizer($filecalls);
 }
 
-if($_GET['ajax']=='file'){
+if($ajax =='file'){
     echo json_encode($filecalls);
     exit;
 }
 
-if($_GET['ajax']=='class'){
-    echo json_encode($finalclasscalls);
-    exit;
-}
-
-if($_GET['ajax'] == 'code') {
-    echo file_get_contents($_GET['file']);
-    exit;
-}
-if($_GET['ajax'] == 'problem') {
-    $file = $_GET['file'];
-    $classparser = new code\codeparser(file_get_contents($_GET['file']),dirname($file));
-    echo json_encode($classparser->getError());
-    exit;
-}
+new overview\overviewvisualizer();
